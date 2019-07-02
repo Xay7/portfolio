fa7fd53a0f5b747ea9bbc0bee9b15929e7b5bc49
Emilian Ćwiok <emilian.cw@gmail.com>
2019-07-02 17:30:26 +0200

    Add about page

---
+++ src/components/About.js
@@ -0,0 +1,67 @@
+import React, { useRef, useEffect } from "react";
+import styled from "styled-components";
+import GuyComputer from "../assets/reactbg.svg";
+
+const Container = styled.div`
+    height: calc(100vh - 55px);
+    width: 100%;
+    padding-top:55px;
+    background-color: white;
+    display:flex;
+`;
+
+const Title = styled.h1`
+    color: black;
+    margin: 0;
+    font-size:48px;
+    white-space: nowrap;
+`;
+
+const Description = styled.h3`
+    color: black;
+    margin: 0;
+    font-size:36px;
+`;
+
+const MeContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    margin-left:10%;
+`;
+
+const ReactImage = styled.img`
+    width:80%;
+    height:80%;
+    object-fit: contain;
+`;
+
+const ImageContainer = styled.div`
+    width: 100%;
+    height: 100%;
+    display: flex;
+    align-items:center;
+    justify-content: center;
+`;
+const About = (props) => {
+
+    let ref = useRef(null);
+
+    useEffect(() => {
+        props.getRef(ref, "about");
+    }, [props])
+
+    return (
+        <Container ref={ref}>
+            <MeContainer>
+                <Title>Hello, I'm Emil</Title>
+                <Description>I code stuff</Description>
+            </MeContainer>
+            <ImageContainer >
+                <ReactImage src={GuyComputer} />
+            </ImageContainer>
+        </Container>
+    )
+}
+
+export default About
