4e81ae09e891a0f5730a0878277c2e486bd4ddde
Emilian Ćwiok <emilian.cw@gmail.com>
2019-07-03 10:32:14 +0200

    Add dynamic css styles based on scroll position

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,7 @@
-import React, { useState } from 'react';
+import React from 'react';
 import styled from 'styled-components';
 import { Home, At, Code } from 'styled-icons/fa-solid/';
-import { Link, withRouter } from 'react-router-dom';
 
-// Containers
 const Nav = styled.nav`
     position:fixed;
     display:flex;
@@ -16,9 +14,7 @@ const Nav = styled.nav`
     border-top: 1px solid rgba(0,0,0,0.1);
 
     @media (min-width: 768px) {
-        height:100vh;
-        width:200px;
-        flex-direction:column;
+        top: 0;
     }
 `;
 
@@ -36,9 +32,6 @@ const NavItemContainer = styled.div`
         color:${props => props.clicked ? "green" : "#AAA"};
         font-size:12px;
         flex-direction: column;
-    }
-    @media (min-width: 768px) {
-        flex-direction: row;
     }
 `;
 
@@ -62,62 +55,73 @@ const ContactIcon = styled(At)`
     transition: all 150ms ease-in-out;
 `;
 
-const StyledLink = styled(Link)`
-    text-decoration: none;
-    width: 100%; 
-    height: 100%;
-    &:focus, &:hover, &:visited, &:StyledLink, &:active {
-        text-decoration: none;
-    }
-    @media (min-width: 768px) {
-        height: auto;
-    }
-`;
+class Navbar extends React.Component {
 
-
-const Navbar = (props) => {
-
-    const [clicked, setClicked] = useState({
-        home: false,
-        projects: false,
-        contact: false
-    })
-
-    // Update clicks when nav item is clicked
-    const handleClick = (item) => {
-        const clicks = clicked;
-        Object.keys(clicks).map(el => {
-            if (el === item) {
-                return clicks[el] = true;
-            } else {
-                return clicks[el] = false;
-            }
-        })
-        setClicked({ ...clicks, });
+    state = {
+        clicked: {
+            about: true,
+            projects: false,
+            contact: false
+        },
+        active: {
+            about: true,
+            projects: false,
+            contact: false
+        }
     }
 
-    return (
-        <Nav>
-            <StyledLink to='/about'>
-                <NavItemContainer onClick={() => handleClick('home')} clicked={clicked.home}>
-                    <HomeIcon clicked={clicked.home} />
+    handleClick = (item, scrolling) => {
+
+        if (item === "about") {
+            window.scrollTo(0, this.props.refs[0].current.offsetTop)
+        }
+        if (item === "projects") {
+            window.scrollTo(0, this.props.refs[1].current.offsetTop - 55)
+        }
+        if (item === "contact") {
+            window.scrollTo(0, this.props.refs[2].current.offsetTop - 55)
+        }
+    }
+
+    handleScroll = () => {
+        if (window.scrollY >= this.props.refs[0].current.offsetTop && window.scrollY < this.props.refs[1].current.offsetTop - 100 && !this.state.active.about) {
+            return this.setState({ active: { about: true, projects: false, contact: false }, clicked: { about: true, projects: false, contact: false } })
+        }
+        else if (window.scrollY >= this.props.refs[1].current.offsetTop - 100 && window.scrollY < this.props.refs[2].current.offsetTop - 100 && !this.state.active.projects) {
+            return this.setState({ active: { about: false, projects: true, contact: false }, clicked: { about: false, projects: true, contact: false } })
+        }
+        else if (window.scrollY >= this.props.refs[2].current.offsetTop - 100 && !this.state.active.contact) {
+            return this.setState({ active: { about: false, projects: false, contact: true }, clicked: { about: false, projects: false, contact: true } })
+        }
+
+    }
+
+    componentDidMount() {
+        document.addEventListener("scroll", this.handleScroll);
+    }
+
+    componentWillMount() {
+        document.removeEventListener("scroll", this.handleScroll);
+    }
+
+    render() {
+        return (
+            <Nav>
+                <NavItemContainer onClick={() => this.handleClick('about')} clicked={this.state.clicked.about}>
+                    <HomeIcon clicked={this.state.clicked.about} />
                     <p>About</p>
                 </NavItemContainer>
-            </StyledLink>
-            <StyledLink to='/projects'>
-                <NavItemContainer onClick={() => handleClick('projects')} clicked={clicked.projects}>
-                    <ProjectsIcon clicked={clicked.projects} />
+                <NavItemContainer onClick={() => this.handleClick('projects')} clicked={this.state.clicked.projects}>
+                    <ProjectsIcon clicked={this.state.clicked.projects} />
                     <p>Projects</p>
                 </NavItemContainer>
-            </StyledLink>
-            <StyledLink to='/contact'>
-                <NavItemContainer onClick={() => handleClick('contact')} clicked={clicked.contact}>
-                    <ContactIcon clicked={clicked.contact} />
+                <NavItemContainer onClick={() => this.handleClick('contact')} clicked={this.state.clicked.contact}>
+                    <ContactIcon clicked={this.state.clicked.contact} />
                     <p>Contact</p>
                 </NavItemContainer>
-            </StyledLink>
-        </Nav >
-    )
+            </Nav >
+        )
+    }
 }
 
-export default withRouter(Navbar);
+export default Navbar;
