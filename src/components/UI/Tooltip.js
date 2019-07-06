227e519bedb3f7ef8bc5f9a280fc3cc2b31ecbfd
Emilian Ćwiok <emilian.cw@gmail.com>
2019-07-06 18:27:33 +0200

    Add triangle to tooltip and animation

--- src/components/UI/Tooltip.js
+++ src/components/UI/Tooltip.js
@@ -1,25 +1,35 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
 const TooltipContainer = styled.div`
-    display: none;
+  visibility: hidden;
+  opacity: 0;
+  position: absolute;
+  width: max-content;
+  padding: 10px;
+  top: -35px;
+  left: 50%;
+  transform: translateX(-50%);
+  height: 20px;
+  background-color: #444444;
+  border-radius: 10px;
+  color: white;
+  transition: all 100ms ease-in-out;
+
+  &::after {
+    content: " ";
     position: absolute;
-    width: max-content;
-    padding: 10px;
-    top: -35px;
-    left: 0;
+    top: 100%;
     left: 50%;
-    transform: translateX(-50%);
-    height: 20px;
-    background-color: black;
-    border-radius: 5px;
-    color: white;
+    margin-left: -5px;
+    border-width: 5px;
+    border-style: solid;
+    border-color: #444444 transparent transparent transparent;
+  }
 `;
 
-const Tooltip = (props) => {
-    return (
-        <TooltipContainer>{props.children}</TooltipContainer>
-    )
-}
+const Tooltip = props => {
+  return <TooltipContainer>{props.children}</TooltipContainer>;
+};
 
 export default Tooltip;
